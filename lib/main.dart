68f52379813aa265ef54164f3c6eb04f9074666a
bhati2633 <bhavya2633@gmail.com>
2024-10-08 20:26:22 -0400

    sound added

--- lib/main.dart
+++ lib/main.dart
@@ -13,40 +13,51 @@ class HalloweenSpookyApp extends StatefulWidget {
 
 class _HalloweenSpookyAppState extends State<HalloweenSpookyApp>
     with SingleTickerProviderStateMixin {
-  final AudioPlayer _audioPlayer = AudioPlayer();
+  final AudioPlayer _backgroundMusicPlayer = AudioPlayer();
+  final AudioPlayer _effectPlayer = AudioPlayer();
   bool _gameWon = false;
   Random _random = Random();
   late AnimationController _controller;
-  late Animation<double> _animation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 10),
-    )..repeat(reverse: true);
-    _animation = Tween<double>(begin: 0, end: 100).animate(_controller);
-    
-    // Play spooky background music in a loop
-    _audioPlayer.setAsset('assets/sounds/spooky_background.mp3');
-    _audioPlayer.setLoopMode(LoopMode.one);
-    _audioPlayer.play();
+      duration: const Duration(seconds: 2),
+    )..repeat();
+
+    _playBackgroundMusic();
+    _moveObjects();
+  }
+
+  Future<void> _playBackgroundMusic() async {
+    try {
+      await _backgroundMusicPlayer.setAsset('assets/sounds/background_music.mp3');
+      await _backgroundMusicPlayer.setLoopMode(LoopMode.one);
+      await _backgroundMusicPlayer.play();
+    } catch (e) {
+      print('Error playing background music: $e');
+    }
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    _audioPlayer.dispose();
+    _backgroundMusicPlayer.dispose();
+    _effectPlayer.dispose();
     super.dispose();
   }
 
   void _playSound(String sound) async {
-    await _audioPlayer.setAsset(sound);
-    _audioPlayer.play();
+    try {
+      await _effectPlayer.setAsset(sound);
+      _effectPlayer.play();
+    } catch (e) {
+      print('Error playing sound: $e');
+    }
   }
 
-  // Handle correct item selection
   void _handleCorrectSelection() {
     setState(() {
       _gameWon = true;
@@ -54,14 +65,21 @@ class _HalloweenSpookyAppState extends State<HalloweenSpookyApp>
     _playSound('assets/sounds/success.mp3');
   }
 
-  // Handle trap selection
   void _handleTrapSelection() {
     _playSound('assets/sounds/jumpscare.mp3');
   }
 
-  // Generate random positions for the objects
   double _randomPosition(double max) {
     return _random.nextDouble() * max;
+  }
+
+  void _moveObjects() {
+    Future.delayed(Duration(seconds: 2), () {
+      if (!_gameWon) {
+        setState(() {});
+        _moveObjects();
+      }
+    });
   }
 
   @override
@@ -73,46 +91,53 @@ class _HalloweenSpookyAppState extends State<HalloweenSpookyApp>
           title: Text("Spooky Halloween Game"),
           backgroundColor: Colors.black,
         ),
-        backgroundColor: Colors.black,
-        body: Stack(
-          children: [
-            if (_gameWon)
-              Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'You Found It!',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.orange,
-                        fontWeight: FontWeight.bold,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Stack(
+            children: [
+              if (_gameWon)
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'You Found It!',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Icon(Icons.star, size: 100, color: Colors.orange),
-                  ],
+                      SizedBox(height: 20),
+                      Icon(Icons.star, size: 100, color: Colors.orange),
+                    ],
+                  ),
+                ),
+              AnimatedPositioned(
+                duration: Duration(seconds: 2),
+                top: _randomPosition(MediaQuery.of(context).size.height - 100),
+                left: _randomPosition(MediaQuery.of(context).size.width - 100),
+                child: GestureDetector(
+                  onTap: _handleTrapSelection,
+                  child: Image.asset('assets/images/ghost1.png', width: 100),
                 ),
               ),
-            AnimatedPositioned(
-              duration: Duration(seconds: 2),
-              top: _randomPosition(MediaQuery.of(context).size.height - 100),
-              left: _randomPosition(MediaQuery.of(context).size.width - 100),
-              child: GestureDetector(
-                onTap: _handleTrapSelection,
-                child: Image.asset('assets/images/ghost.jpg', width: 100),
+              AnimatedPositioned(
+                duration: Duration(seconds: 2),
+                top: _randomPosition(MediaQuery.of(context).size.height - 100),
+                left: _randomPosition(MediaQuery.of(context).size.width - 100),
+                child: GestureDetector(
+                  onTap: _handleCorrectSelection,
+                  child: Image.asset('assets/images/pumpkin.png', width: 100),
+                ),
               ),
-            ),
-            AnimatedPositioned(
-              duration: Duration(seconds: 2),
-              top: _randomPosition(MediaQuery.of(context).size.height - 100),
-              left: _randomPosition(MediaQuery.of(context).size.width - 100),
-              child: GestureDetector(
-                onTap: _handleCorrectSelection,
-                child: Image.asset('assets/images/pumpkin.png', width: 100),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
